1808e78db639d21cc7185fd989417bb8898ac389
Francis Sepnio Jr <sepniojr@cs.ualberta.ca>
2023-02-04 13:42:08 -0700

    edited single semaphore server

--- singleSemServer.cpp
+++ singleSemServer.cpp
@@ -6,20 +6,47 @@
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<pthread.h>
+#include<semaphore.h>
+
+extern "C" {
+    #include "common.h"
+    #include "timer.h"
+}
+
+char **theArray;
+sem_t semaphore;
 
 void *ServerEcho(void *args)
 {
     int clientFileDescriptor=(int)args;
-    char str[20];
-
-    read(clientFileDescriptor,str,20);
+    char str[COM_BUFF_SIZE]; // array to hold client string
+    char dst[COM_BUFF_SIZE];
+    read(clientFileDescriptor,str, COM_BUFF_SIZE); // read client string into str array
     printf("reading from client:%s\n",str);
+
+    ClientRequest rqst;
+    // Tokenize string data and set it to object attributes
+    ParseMsg(str, &rqst); 
+
+    // CRITICAL SECTION
+    sem_wait(&semaphore);
+    if (rqst.is_read == 0){
+        // Write case -> server will update corresponding string in array with new text supplied by client.
+        // Server will then send the updated string from array to the client
+        setContent(str, rqst.pos, theArray);
+    }
+    // Read case -> server will just send back the corresponding string to the client
+    getContent(dst, rqst.pos, theArray);
+
+    sem_post(&semaphore);
+
+
     write(clientFileDescriptor,str,20);
     close(clientFileDescriptor);
     return NULL;
 }
 
-
+// size of string array, server ip, server port
 int main(int argc, char* argv[])
 {
     struct sockaddr_in sock_var;
@@ -28,14 +55,25 @@ int main(int argc, char* argv[])
     int i;
     pthread_t t[20];
 
-    sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
-    sock_var.sin_port=3000;
+    //allocating "n" amount of space for theArray
+    //https://stackoverflow.com/questions/4316987/define-the-size-of-a-global-array-from-the-command-line
+    int elements = atoi (argv [1]);
+    theArray = (char**)malloc (elements * sizeof (theArray[0]));
+
+    // changed socket commands to implement command line arguments
+    sock_var.sin_addr.s_addr=inet_addr(argv[2]); 
+    sock_var.sin_port= atoi(argv[3]);
     sock_var.sin_family=AF_INET;
+
+    // Initialize our semaphore
+    sem_init(&semaphore, 0, 1);
+
     if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
     {
         printf("socket has been created\n");
-        listen(serverFileDescriptor,2000); 
-        while(1)        //loop infinity
+        listen(serverFileDescriptor, COM_NUM_REQUEST); //changed '2000' to COM_NUM_REQUEST
+
+        while(1)        //loop infinity, wait for client connections
         {
             for(i=0;i<20;i++)      //can support 20 clients at a time
             {
