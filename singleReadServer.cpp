561497b50dfd44bccc615fffe87f9b598ba23e4b
FrancisSepnioJr <sepniojr@ualberta.ca>
2023-02-07 11:33:19 -0700

    added comments

--- singleReadServer.cpp
+++ singleReadServer.cpp
@@ -15,8 +15,8 @@ extern "C" {
     #include "timer.h"
 }
 
+// Initialize global variables
 char **theArray;
-
 double timeStart;
 double timeEnd;
 double timeList[COM_NUM_REQUEST] = {0};
@@ -38,46 +38,50 @@ void *ServerEcho(void *args)
 
     // Tokenize string data and set it to object attributes
     ParseMsg(str, &rqst); 
-    
-    // CRITICAL SECTION
 
     if (rqst.is_read == 0){
         // Write case -> server will update corresponding string in array with new text supplied by client.
-
         GET_TIME(timeStart);
 
-        pthread_rwlock_wrlock(&readwritelock); // Write lock -> no other threads may write to the array
-  
-        // Server will then send the updated string from array to the client
+        // Write lock -> no other threads may write to the array
+        pthread_rwlock_wrlock(&readwritelock); 
+
         setContent(rqst.msg, rqst.pos, theArray);
+          
+        // Server will then send the updated string from array to the client
         getContent(str, rqst.pos, theArray);
+        // Remove the write lock
         pthread_rwlock_unlock(&readwritelock);
+
         GET_TIME(timeEnd);
-        if (write(clientFileDescriptor,str,COM_BUFF_SIZE) < 0){
-            cout << "server write error" << endl;
-        }
+
+        // Write the string to the client fd
+        write(clientFileDescriptor,str,COM_BUFF_SIZE);
 
     } else {
-        GET_TIME(timeStart);
         // Read case -> server will just send back the corresponding string to the client
         // Blocks if a thread holds the lock for writing and no threads are waiting on the lock
-        pthread_rwlock_rdlock(&readwritelock);  // Read lock -> no other threads here may read from the array
-
+        GET_TIME(timeStart);
+        
+        // Read lock -> no other threads here may read from the array
+        pthread_rwlock_rdlock(&readwritelock); 
         getContent(dst, rqst.pos, theArray); // save string in position 'pos' from theArray to dst
+        // Remove the read lock
         pthread_rwlock_unlock(&readwritelock);
 
         GET_TIME(timeEnd);
-        if (write(clientFileDescriptor,dst,COM_BUFF_SIZE) < 0){
-            cout << "server read error" << endl;
-        }
+        
+        // Write the string to the client fd
+        write(clientFileDescriptor,dst,COM_BUFF_SIZE);
 
     }
 
+    // Lock the index so we can save the time to the correct array index
     pthread_mutex_lock(&indexlock);
     timeList[request] = timeEnd - timeStart;
     request++;
     pthread_mutex_unlock(&indexlock);
-    // END CRITICAL SECTION
+
     close(clientFileDescriptor);
     return NULL;
 }
@@ -89,12 +93,16 @@ int main(int argc, char* argv[])
     int serverFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
     int clientFileDescriptor;
     int i;
+    
+    // Array of threads to hold COM_NUM_REQUEST threads
     pthread_t t[COM_NUM_REQUEST];
     pthread_rwlock_init(&readwritelock,NULL);
 
     //allocating "n" amount of space for theArray
     //https://stackoverflow.com/questions/4316987/define-the-size-of-a-global-array-from-the-command-line
     int elements = atoi(argv[1]);
+
+    // Allocate space for our string array
     theArray = (char**)malloc (elements * sizeof(theArray[0]));
     for (int i = 0; i < elements; i++){
         theArray[i] = (char*)malloc(COM_BUFF_SIZE * sizeof(char));
@@ -109,11 +117,11 @@ int main(int argc, char* argv[])
     if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
     {
         printf("socket has been created\n");
-        listen(serverFileDescriptor, COM_NUM_REQUEST); //changed '2000' to COM_NUM_REQUEST
+        listen(serverFileDescriptor, COM_NUM_REQUEST); 
 
-        while(1)        //loop infinity, wait for client connections
+        while(1)
         {
-            for(i=0;i<COM_NUM_REQUEST;i++)      //change '20' to COM_NUM_REQUEST support 20 clients at a time
+            for(i=0;i<COM_NUM_REQUEST;i++)      // Loop for COM_NUM_REQUEST threads
             {
                 clientFileDescriptor=accept(serverFileDescriptor,NULL,NULL);
                 printf("Connected to client %d\n",clientFileDescriptor);
@@ -122,6 +130,7 @@ int main(int argc, char* argv[])
             for (i=0; i<COM_NUM_REQUEST; i++){
                 pthread_join(t[i], NULL);
             }
+            // Save the average memory access latency to process 1000 requests
             saveTimes(timeptr, COM_NUM_REQUEST);
 
         }
@@ -133,6 +142,7 @@ int main(int argc, char* argv[])
     for (int i = 0; i < elements; i++){
         free(theArray[i]);
     }
+    // Free space and destroy locks
     free(theArray);
     pthread_rwlock_destroy(&readwritelock);
     return 0;
